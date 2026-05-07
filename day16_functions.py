ff72a702327e3d0e1f8e95362c7a56770ecba61a
Abdul Rahman <rahmann292006@gmail.com>
2026-05-07 09:18:06 +0530

    Add functions for greeting and squaring a number

---
+++ day16_functions.py
@@ -0,0 +1,13 @@
+# Day 16 - Functions with Parameters
+
+def greet(name):
+    print("Welcome", name)
+
+def square(num):
+    return num * num
+
+greet("Abdul")
+
+result = square(5)
+
+print("Square:", result)
