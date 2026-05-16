fc946123486bbe05f6507012f6e0b5c1e5c058e8
Abdul Rahman <rahmann292006@gmail.com>
2026-05-16 07:59:40 +0530

    Add simple calculator with math operations using python modules

---
+++ Pymodules_simple _calculator.py
@@ -0,0 +1,24 @@
+# Day 27 - Modules and Calculator
+
+import math
+
+# Square root
+number = 25
+print("Square Root:", math.sqrt(number))
+
+# Power
+print("Power:", math.pow(2, 5))
+
+# Ceiling value
+print("Ceiling:", math.ceil(4.3))
+
+# Simple Calculator
+
+a = 10
+b = 5
+
+print("\nCalculator Operations")
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
